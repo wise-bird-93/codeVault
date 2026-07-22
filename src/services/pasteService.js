93972ee3c7813b27dc23ce744e1c363b9ef0c053
wise-bird-93 <yuvrajdhingra303@gmail.com>
2026-07-22 13:40:25 +0530

    Configure production API URL

--- src/services/pasteService.js
+++ src/services/pasteService.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/pastes",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getAllPastes = () => API.get("/");
-
 export const getPasteById = (id) => API.get(`/${id}`);
-
 export const createPaste = (data) => API.post("/", data);
-
 export const updatePaste = (id, data) => API.put(`/${id}`, data);
-
 export const deletePaste = (id) => API.delete(`/${id}`);
